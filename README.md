a6cf6b4e8072f8ea533db0097bda594b0e1bd5cf
martin.vallone <martin.vallone@fiqus.com>
2021-08-04 17:19:52 -0300

    adding some files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # phruzz-matcher
-Combination of the RapidFuzz library with Spacy PhraseMatcher 
+> Combination of the RapidFuzz library with Spacy PhraseMatcher 
+
+## Installation
+
+        git clone https://github.com/mjvallone/phruzz_matcher_spacy.git
+
+## Configuration
+
+1. Create virtualenv using python3 (follow https://virtualenvwrapper.readthedocs.io/en/latest/install.html)
+
+        virtualenv venv
+
+2. Activate the virtualenv
+
+        . venv/bin/activate
+
+3. Install requirements
+
+        pip install -r requirements.txt
+
+4. Download a spanish base model (we recommend the large one)
+
+        python -m spacy download es_core_news_lg
+
+## Using it!
+
+        Open a console and run the command
+                `python [function you want to use] [parameters for that function]`
---
+++ phruzz_matcher.py
@@ -0,0 +1,64 @@
+import re
+from rapidfuzz import fuzz
+from spacy.matcher import PhraseMatcher
+from spacy.tokens import Span, Doc
+from spacy.util import filter_spans
+from spacy.language import Language
+MATCH_PERCENTAGE = 92
+
+
+nlp = spacy.blank('es')
+
+class PhruzzMatcher:
+    name = "phruzz_matcher"
+
+    def __init__(self, nlp: Language, phrases_list, entity_label):
+        self.phrases_list = phrases_list
+        self.entity_label = entity_label
+        self.matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
+        patterns = list(nlp.tokenizer.pipe(phrases_list))
+        self.matcher.add(f"{entity_label.lower().replace(' ', '_')}_list", patterns)
+
+    def __call__(self, doc: Doc) -> Doc:
+        matches = self.matcher(doc)
+
+        if matches:
+            # using PhraseMatcher from Spacy
+            match = matches[0]
+            start = match[-2]
+            end = match[-1]
+        else:
+            # using RapidFuzz to find matches when there were NO "perfect matches" due to typos or abreviations
+            # consider that MATCH_PERCENTAGE is the percentage from the one you will keep matches between
+            # what is written in the text and the list of phrases
+            # higher the percentage, lower the differences "tolerated" to find a match
+            tokens = [token.text for token in doc]
+            match = fuzzy_matcher(self.phrases_list, tokens, MATCH_PERCENTAGE)
+            if match:
+                start = match[-2]
+                end = match[-1] + 1
+
+        if match:
+            doc.ents = filter_spans([Span(doc, start, end, label=self.entity_label)] + list(doc.ents))
+
+        return doc
+
+
+def fuzzy_matcher(features, tokens, match=None):
+    matches = []
+    for feature in features:
+        feature_length = len(feature.split(" "))
+        for i in range(len(tokens) - feature_length + 1):
+            matched_phrase = ""
+            j = 0
+            for j in range(i, i + feature_length):
+                if re.search(r"[,!?{}\[\]]", tokens[j]):
+                    break
+                matched_phrase = matched_phrase + " " + tokens[j].lower()
+            matched_phrase.strip()
+            if not matched_phrase == "":
+
+                if fuzz.ratio(matched_phrase, feature.lower()) > match:
+                    matches.append([matched_phrase, feature, i, j])
+
+    return matches[0] if len(matches) else matches
