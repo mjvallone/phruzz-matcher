5817176a4ca66dea1bc5b39b9f20c669226c11d5
martin.vallone <martin.vallone@fiqus.com>
2021-09-16 16:55:46 -0300

    refactoring code

--- phruzz_matcher/phrase_matcher.py
+++ phruzz_matcher/phrase_matcher.py
@@ -1,20 +1,44 @@
-import spacy
+# import spacy
+import re
+
 from spacy.matcher import PhraseMatcher
 from spacy.tokens import Span, Doc
 from spacy.util import filter_spans
 from spacy.language import Language
-from utils import fuzzy_matcher
-MATCH_PERCENTAGE = 92 #TODO deberia recibir match_percentage
+from rapidfuzz import fuzz
+
+MATCH_PERCENTAGE = 92
 
 
-nlp = spacy.blank('es') #FIXME eliminar una vez probado que recibe un modelo
+# nlp = spacy.blank('es') #FIXME eliminar una vez probado que recibe un modelo
+
+def fuzzy_matcher(features, tokens, match=None):
+    matches = []
+    for feature in features:
+        feature_length = len(feature.split(" "))
+        for i in range(len(tokens) - feature_length + 1):
+            matched_phrase = ""
+            j = 0
+            for j in range(i, i + feature_length):
+                if re.search(r"[,!?{}\[\]]", tokens[j]):
+                    break
+                matched_phrase = matched_phrase + " " + tokens[j].lower()
+            matched_phrase.strip()
+            if not matched_phrase == "":
+
+                if fuzz.ratio(matched_phrase, feature.lower()) > match:
+                    matches.append([matched_phrase, feature, i, j])
+
+    return matches[0] if len(matches) else matches
+
 
 class PhruzzMatcher:
     name = "phruzz_matcher"
 
-    def __init__(self, nlp: Language, phrases_list, entity_label):
+    def __init__(self, nlp: Language, phrases_list, entity_label, match_percentage=None):
         self.phrases_list = phrases_list
         self.entity_label = entity_label
+        self.match_percentage = match_percentage or MATCH_PERCENTAGE
         self.matcher = PhraseMatcher(nlp.vocab, attr="LOWER")
         patterns = list(nlp.tokenizer.pipe(phrases_list))
         self.matcher.add(f"{entity_label.lower().replace(' ', '_')}_list", patterns)
@@ -33,7 +57,7 @@ class PhruzzMatcher:
             # what is written in the text and the list of phrases
             # higher the percentage, lower the differences "tolerated" to find a match
             tokens = [token.text for token in doc]
-            match = fuzzy_matcher(self.phrases_list, tokens, MATCH_PERCENTAGE)
+            match = fuzzy_matcher(self.phrases_list, tokens, self.match_percentage)
             if match:
                 start = match[-2]
                 end = match[-1] + 1
